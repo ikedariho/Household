75bc3cfb6ae29716c26d2c4f99d0c9700c19222c
ikedariho <riho.ikeda@rakus-partners.co.jp>
2019-12-16 17:09:32 +0900

    給与計算作成

---
+++ src/main/java/com/example/controller/SalaryController.java
@@ -0,0 +1,50 @@
+package com.example.controller;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.domain.Salary;
+import com.example.form.SalaryForm;
+import com.example.service.SalaryService;
+
+/**
+ * 給与登録するコントローラ.
+ * 
+ * @author riho.ikeda
+ *
+ */
+@Controller
+@RequestMapping("/")
+public class SalaryController {
+
+	@Autowired
+	private SalaryService salaryService;
+
+	@RequestMapping("")
+	public String index(Model model) {
+		
+		
+		
+		return "salary_calculation";
+	}
+
+	/**
+	 * 給与情報を登録する.
+	 * 
+	 * @param salaryForm
+	 * @param model
+	 * @return
+	 */
+	@RequestMapping("/salaryInsert")
+	public String salaryInsert(SalaryForm salaryForm, Model model) {
+		Salary salary = new Salary();
+		BeanUtils.copyProperties(salaryForm, salary);
+		salaryService.salaryInsert(salary);
+		return "salary_detail";
+
+	}
+
+}
---
+++ src/main/java/com/example/service/SalaryService.java
@@ -0,0 +1,33 @@
+package com.example.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.domain.Salary;
+import com.example.repository.SalaryRepository;
+
+/**
+ * 給与登録するサービス.
+ * 
+ * @author riho.ikeda
+ *
+ */
+@Service
+@Transactional
+public class SalaryService {
+
+	@Autowired
+	private SalaryRepository salaryRepository;
+
+	/**
+	 * 給与登録する.
+	 * 
+	 * @param salary
+	 */
+	public void salaryInsert(Salary salary) {
+		salaryRepository.insert(salary);
+
+	}
+
+}

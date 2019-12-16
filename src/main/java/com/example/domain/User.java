226dc298687e0ba97e0cc6e606ec12cfcd7be7e4
yosukeYamada <yamada_141@yahoo.co.jp>
2019-12-16 14:01:31 +0900

    userドメインを修正.dateをDate型からTimestamp型のtimestampに変更

--- src/main/java/com/example/domain/User.java
+++ src/main/java/com/example/domain/User.java
@@ -1,6 +1,6 @@
 package com.example.domain;
 
-import java.util.Date;
+import java.security.Timestamp;
 
 /**
  * ユーザー登録に使用するドメイン.
@@ -9,65 +9,72 @@ import java.util.Date;
  *
  */
 public class User {
-	/* ID*/
+	/* ID */
 	private Integer id;
-	/* 男性名前*/
+	/* 男性名前 */
 	private String manName;
-	/* 女性名前*/
+	/* 女性名前 */
 	private String womanName;
-	/* UserID*/
+	/* UserID */
 	private String userId;
-	/* パスワード*/
+	/* パスワード */
 	private String password;
-	/* 登録日付*/
-	private Date date;
+	/* 登録日付 */
+	private Timestamp timestamp;
+
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	public String getManName() {
 		return manName;
 	}
+
 	public void setManName(String manName) {
 		this.manName = manName;
 	}
+
 	public String getWomanName() {
 		return womanName;
 	}
+
 	public void setWomanName(String womanName) {
 		this.womanName = womanName;
 	}
+
 	public String getUserId() {
 		return userId;
 	}
+
 	public void setUserId(String userId) {
 		this.userId = userId;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	public Date getDate() {
-		return date;
+
+	public Timestamp getTimestamp() {
+		return timestamp;
 	}
-	public void setDate(Date date) {
-		this.date = date;
+
+	public void setTimestamp(Timestamp timestamp) {
+		this.timestamp = timestamp;
 	}
-	/* (non-Javadoc)
-	 * @see java.lang.Object#toString()
-	 */
+
 	@Override
 	public String toString() {
 		return "User [id=" + id + ", manName=" + manName + ", womanName=" + womanName + ", userId=" + userId
-				+ ", password=" + password + ", date=" + date + "]";
+				+ ", password=" + password + ", timestamp=" + timestamp + "]";
 	}
 	
 	
-	
-	
-
 }

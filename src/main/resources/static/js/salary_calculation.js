aaa8dc2ce183a970ab048ac9a277fd3050b50e32
ikedariho <riho.ikeda@rakus-partners.co.jp>
2019-12-17 10:04:14 +0900

    リアルタイムで計算可能

--- src/main/resources/static/js/salary_calculation.js
+++ src/main/resources/static/js/salary_calculation.js
@@ -3,30 +3,19 @@
  */
 
 $(function() {
-	calc_price();
-	$(".womanSalary").on("input", function() {
-		calc_price();
-	});
 
-	$(".manSalary").on("input", function() {
-		calc_price();
-	});
-
-	function calc_price() {
-		var manText = $('.manSalary [name=manSalary]').val();
+	$(".Salary").keyup(function() {
+		var manText = $('input[name=manSalary]').val();
 		var manSalary = Number(manText) || 0; // NaNのとき0にする
 		console.log(manSalary);
-		var womanText = $('.womanSalary [name=womanSalary]').val();
-		var womanSalary = Number(manText) || 0; // NaNのとき0にする
+		var womanText = $('input[name=womanSalary]').val();
+		var womanSalary = Number(womanText) || 0; // NaNのとき0にする
 		console.log(womanSalary);
 
 		var totalSalary = (manSalary + womanSalary);
-		$("#totalSalary").text(price.toLocaleString());
+		$("#totalSalary").text(totalSalary.toLocaleString());
 		console.log(totalSalary);
-		
-	}
-	;
 
-});/**
-	 * 
-	 */
+	});
+
+});

2cc7922301f6893b4cfbba842856e56209cad12c
yosukeYamada <yamada_141@yahoo.co.jp>
2019-12-18 10:34:18 +0900

    HTMLのレイアウトを修正

--- src/main/java/com/example/service/RegisterBudgetService.java
+++ src/main/java/com/example/service/RegisterBudgetService.java
@@ -1,8 +1,6 @@
 package com.example.service;
 
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
--- src/main/resources/static/js/living_budget.js
+++ src/main/resources/static/js/living_budget.js
@@ -4,12 +4,8 @@
 $(function() {
 	var i = 3;
 	$("#add_category_btn").click(function() {
-		if(i<=10){
-			if(i<10){
-				$("#category_ul").append("\<li\>項目" + i + "&nbsp;&nbsp;&nbsp;\<input type=&quot;text&quot; name=categoryNameList\>  予算 \<input type=&quot;text&quot; name=budgetList\>\<\/li\>");
-			}else{
-				$("#category_ul").append("\<li\>項目" + i + "&nbsp;\<input type=&quot;text&quot; name=categoryNameList\>  予算 \<input type=&quot;text&quot; name=budgetList\>\<\/li\>");
-			}
+		if(i<=8){
+				$("#category_ul").append("\<li\>項目" + i + "&nbsp;&nbsp;&nbsp;<input type=&quot;text&quot; name=categoryNameList\>  予算 \<input type=&quot;text&quot; name=budgetList\>\<\/li\>");
 			i++;
 			}else{
 				alert('これ以上項目を追加できません');
